d8806c1463910cb3ff37f2025ed578bb835f1e74
Clintdevweb <hanhdevit@gmail.com>
2022-03-20 14:49:51 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,20 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public name = 'huy'
-  public age = 15
+  public name = 'huy';
+  public age = 15;
+  public fruits = ['apple', 'mango', 'banana'];
+  public fruits2 = [
+    { ten: 'apple', gia: 100 , sale: true},
+    { ten: 'mango', gia: 100 , sale: true},
+    { ten: 'banana', gia: 100, sale: false },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.fruits);
   }
 
   public resetValue(): void {
-    this.name = ''
-    this.age = 0
-
+    this.name = '';
+    this.age = 0;
   }
 }
